ab5447f8b0e039700c8e0257062a63f3f17332ed
Muhamed Musammil P <p.musammil98@gmail.com>
2022-08-27 21:11:32 +0530

    initial project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginPage from './login/login'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+ <LoginPage/>
+</header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/login/login.jsx
@@ -0,0 +1,12 @@
+
+import React, { useState } from 'react';
+function LoginPage() {
+  return (
+    <div>
+      <span>Testing ...</span>
+       
+    </div>
+  );
+}
+
+export default LoginPage;
